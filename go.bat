ed4061c20a9ec374404116f371b0d50500704534
yinxiujuan <1204927037@qq.com>
2019-11-25 14:14:18 +0800

    龚熙海-1740610471-提交修改cms任务需求分析书

--- go.bat
+++ go.bat
@@ -1,1 +1,1 @@
-git status  && git add -A && git commit -m "李文杰-1740610470-提交修改cms任务需求分析书" && git push -u origin master:master 
+git status  && git add -A && git commit -m "龚熙海-1740610471-提交修改cms任务需求分析书" && git push -u origin master:dev
