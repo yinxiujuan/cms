511710a50bc6a2b1a7979be8657c11fd21a1ca3b
morly <morlyqiao@qq.com>
2019-11-25 22:57:47 +0800

    付阳林-1740610372-对test.c代码段进行再次修改

--- test.c
+++ test.c
@@ -10,7 +10,7 @@ int rawdata_to_little_endian(uint8_t *origindata,
     uint32_t row_num, uint32_t row_size) {
     gf_error_t err = GF_SUCCESS;
     uint32_t row = 0;
-    uint32_t i = 0;
+    uint32_t i = 0;     
     uint32_t index = 0;
     uint8_t *buffer = NULL;
     FUNC_ENTER();
