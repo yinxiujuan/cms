51cca86603d6cac710447ceddb5039697a7a90bc
Morly <1255394075@qq.com>
2019-11-28 21:31:27 +0800

    刘巧使用SVN对文件进行修改

--- go.bat
+++ go.bat
@@ -1,1 +1,1 @@
-git status  && git add -A && git commit -m "龚熙海-1740610471-提交修改cms任务需求分析书" && git push -u origin master:dev
+git status  && git add -A && git commit -m "刘巧-1740610373-提交修改cms任务需求分析书" && git push -u origin master:dev
